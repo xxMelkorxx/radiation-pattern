cef90fc8c15be0e65e383e1231b72e692d9f0383
SerBor <xxxserborxxx@gmail.com>
2022-10-12 16:56:01 +0300

    initial commit

---
+++ radiation-pattern/MainForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace radiation_pattern
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
